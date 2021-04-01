3148d90fc208bbe6ca9446b6c64f1dddf4ffbdb9
foxfoxff <huyaohyh@qq.com>
2021-04-01 23:04:10 +0800

    finish cart

---
+++ src/VIEW/cart.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import 'antd/dist/antd.css'
+import { Input } from 'antd';
+
+import { BackTop } from 'antd';
+import { Layout } from 'antd';
+import Sidemenu from "../component/sidemenu";
+import Booktable from "../component/booktable"
+import Upnav from "../component/upnav";
+import Cartlist from "../component/Cartlist";
+
+const { Header, Footer, Sider, Content } = Layout;
+const { Search } = Input;
+
+
+
+export default class Cart extends React.Component {
+
+
+    render() {
+
+        return (
+
+            <Layout >
+
+                <Header >
+                    <Upnav />
+                </Header>
+                <BackTop />
+                <Layout>
+                    <Sider theme={"light"}>
+                        <Sidemenu />
+                    </Sider>
+                    <div >
+                        <Cartlist />
+                    </div>
+
+
+                </Layout>
+
+
+
+
+            </Layout>
+
+
+
+
+
+        )
+    }
+}
